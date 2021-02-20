338504c2271e5940d771c5b6f7282caea013d020
Shandy Siswandi <shandysiswandi@gmail.com>
2021-02-21 06:08:48 +0700

    create cmd/console main.go

---
+++ cmd/console/main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"go-service-gin/application"
+	"go-service-gin/config"
+	"go-service-gin/infrastructure/database"
+	"go-service-gin/infrastructure/database/mysql"
+	"go-service-gin/interfaces/console"
+	"go-service-gin/util/logger"
+	"sync"
+	"time"
+
+	"github.com/joho/godotenv"
+)
+
+var wg = sync.WaitGroup{}
+
+func main() {
+	/********** ********** ********** ********** ********** ********** ********** ********** ********** **********/
+	/* load environment variable
+	/********** ********** ********** ********** ********** ********** ********** ********** ********** **********/
+	if err := godotenv.Load(); err != nil {
+		logger.LogError(err)
+	}
+
+	/********** ********** ********** ********** ********** ********** ********** ********** ********** **********/
+	/* define configuration
+	/********** ********** ********** ********** ********** ********** ********** ********** ********** **********/
+	var (
+		dbConfig = config.NewDatabaseConfig()
+	)
+
+	/********** ********** ********** ********** ********** ********** ********** ********** ********** **********/
+	/* define database connection
+	/********** ********** ********** ********** ********** ********** ********** ********** ********** **********/
+	db, err := database.NewDatabase(dbConfig)
+	if err != nil {
+		logger.LogError(err)
+	}
+
+	/********** ********** ********** ********** ********** ********** ********** ********** ********** **********/
+	/* define variable and inject to constructor
+	/********** ********** ********** ********** ********** ********** ********** ********** ********** **********/
+	var (
+		blogRepository = mysql.NewBlogRepository(db)
+		blogLogic      = application.NewBlogApplication(blogRepository)
+		blogConsole    = console.NewBlogConsole(blogLogic)
+	)
+
+	// testing. stress test
+	t := time.Now()
+	for i := 1; i <= 5; i++ {
+		blogConsole.Create()
+	}
+	logger.LogInfo(time.Since(t))
+}
