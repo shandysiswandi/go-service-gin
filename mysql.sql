0eaa4cc57f7be6677bd3e766d4099ead3d907a28
Shandy Siswandi <shandysiswandi@gmail.com>
2021-02-21 05:15:36 +0700

    alter type column body

--- mysql.sql
+++ mysql.sql
@@ -4,7 +4,7 @@ DROP TABLE IF EXISTS `blogs`;
 CREATE TABLE `blogs` (
   `id` char(36) NOT NULL,
   `title` varchar(100) NOT NULL,
-  `body` varchar(100) NOT NULL,
+  `body` varchar(1000) NOT NULL,
   `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   `deleted_at` timestamp NULL DEFAULT NULL,
