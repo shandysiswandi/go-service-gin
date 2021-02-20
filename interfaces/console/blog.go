b30efdec32b6583470f4d28ff628e0110abad017
Shandy Siswandi <shandysiswandi@gmail.com>
2021-02-21 05:16:19 +0700

    rename to BlogConsole

--- interfaces/console/blog.go
+++ interfaces/console/blog.go
@@ -4,36 +4,36 @@ import (
 	"go-service-gin/application"
 	"go-service-gin/domain/blogs"
 	"go-service-gin/util/faker"
+	"go-service-gin/util/logger"
 	"go-service-gin/util/uuid"
 )
 
-// BlogREST is
-type BlogREST struct {
+// BlogConsole is
+type BlogConsole struct {
 	blogLogic *application.BlogApplication
 }
 
-// NewBlogREST is
-func NewBlogREST(ba *application.BlogApplication) *BlogREST {
+// NewBlogConsole is
+func NewBlogConsole(ba *application.BlogApplication) *BlogConsole {
 	if ba == nil {
 		return nil
 	}
 
-	return &BlogREST{ba}
+	return &BlogConsole{ba}
 }
 
 // Create is
-func (bh *BlogREST) Create(run int) bool {
-	b := &blogs.Blog{}
-
+func (bh *BlogConsole) Create(run int) bool {
 	for i := 1; i <= run; i++ {
-		b = &blogs.Blog{
+		b := &blogs.Blog{
 			ID:    uuid.Generate(),
 			Title: faker.Sentence(),
 			Body:  faker.Paragraph(),
 		}
 
 		if err := bh.blogLogic.Create(b); err != nil {
-			return false
+			logger.LogInfo(err)
+			continue
 		}
 	}
 
