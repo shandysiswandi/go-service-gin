0f0c09920eed7ca344f27e2ef7745f13028d10b1
Shandy Siswandi <shandysiswandi@gmail.com>
2021-02-21 14:29:42 +0700

    add field release

--- config/sentry.go
+++ config/sentry.go
@@ -4,14 +4,16 @@ import "os"
 
 // SentryConfig is
 type SentryConfig struct {
-	DSN string
-	ENV string
+	DSN     string
+	ENV     string
+	Release string
 }
 
 // NewSentryConfig is
 func NewSentryConfig() *SentryConfig {
 	return &SentryConfig{
-		DSN: os.Getenv("SENTRY_DSN"),
-		ENV: os.Getenv("SENTRY_ENV"),
+		DSN:     os.Getenv("SENTRY_DSN"),
+		ENV:     os.Getenv("SENTRY_ENV"),
+		Release: os.Getenv("SENTRY_RELEASE"),
 	}
 }
