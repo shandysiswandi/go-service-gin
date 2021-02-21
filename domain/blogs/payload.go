25edb5d8aff164c89f5ed05240173f62ff16c4b2
Shandy Siswandi <shandysiswandi@gmail.com>
2021-02-21 16:53:18 +0700

    add payload CreateBlog & UpdateBlog

--- domain/blogs/payload.go
+++ domain/blogs/payload.go
@@ -1,1 +1,18 @@
 package blogs
+
+import "time"
+
+// CreateBlog is
+type CreateBlog struct {
+	ID    string `json:"id" bson:"id"`
+	Title string `json:"title" bson:"title"`
+	Body  string `json:"body" bson:"body"`
+}
+
+// UpdateBlog is
+type UpdateBlog struct {
+	ID        string    `json:"id" bson:"id"`
+	Title     string    `json:"title" bson:"title"`
+	Body      string    `json:"body" bson:"body"`
+	UpdatedAt time.Time `json:"updated_at" bson:"updated_at"`
+}
