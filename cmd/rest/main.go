5dcf9e5646cd1411bcb3a70620dabb6a960b3ecb
Shandy Siswandi <shandysiswandi@gmail.com>
2021-02-21 15:25:27 +0700

    improve rest/main.go

--- cmd/rest/main.go
+++ cmd/rest/main.go
@@ -2,11 +2,9 @@ package main
 
 import (
 	"fmt"
+	app "go-service-gin"
 	"go-service-gin/application"
 	"go-service-gin/config"
-	"go-service-gin/infrastructure/database"
-	"go-service-gin/infrastructure/database/mysql"
-	"go-service-gin/infrastructure/library/redis"
 	"go-service-gin/infrastructure/library/sentry"
 	"go-service-gin/interfaces/rest"
 	"go-service-gin/util/logger"
@@ -15,69 +13,49 @@ import (
 	"github.com/gin-contrib/cors"
 	"github.com/gin-contrib/gzip"
 	"github.com/gin-gonic/gin"
-	"github.com/joho/godotenv"
 )
 
 func main() {
-	/********** ********** ********** ********** ********** ********** ********** ********** ********** **********/
-	/* load environment variable
-	/********** ********** ********** ********** ********** ********** ********** ********** ********** **********/
-	if err := godotenv.Load(); err != nil {
-		logger.LogError(err)
-	}
-
-	/********** ********** ********** ********** ********** ********** ********** ********** ********** **********/
-	/* define configuration
-	/********** ********** ********** ********** ********** ********** ********** ********** ********** **********/
+	/* load app data and typed and define variable hanlder and inject data to constructor */
 	var (
-		appConfig    = config.NewAppConfig()
-		dbConfig     = config.NewDatabaseConfig()
-		redisConfig  = config.NewRedisConfig()
-		sentryConfig = config.NewSentryConfig()
-	)
+		appData   = app.New()
+		appConfig = appData[app.AppConfig].(*config.AppConfig)
+		sentry    = appData[app.Sentry].(*sentry.Sentry)
+		blogLogic = appData[app.BlogLogic].(*application.BlogApplication)
 
-	/********** ********** ********** ********** ********** ********** ********** ********** ********** **********/
-	/* define database connection
-	/********** ********** ********** ********** ********** ********** ********** ********** ********** **********/
-	db, err := database.NewDatabase(dbConfig)
-	if err != nil {
-		logger.LogError(err)
-	}
-
-	/********** ********** ********** ********** ********** ********** ********** ********** ********** **********/
-	/* define library
-	/********** ********** ********** ********** ********** ********** ********** ********** ********** **********/
-	redis := redis.New(redisConfig)
-	sentry := sentry.New(sentryConfig)
-
-	logger.LogInfo(redis)
-
-	/********** ********** ********** ********** ********** ********** ********** ********** ********** **********/
-	/* define variable and inject to constructor
-	/********** ********** ********** ********** ********** ********** ********** ********** ********** **********/
-	var (
-		// default
 		defaultHandler = rest.NewDefaultREST(sentry)
-
-		// blogs
-		blogRepository = mysql.NewBlogRepository(db)
-		blogLogic      = application.NewBlogApplication(blogRepository)
 		blogHandler    = rest.NewBlogREST(blogLogic)
 	)
 
-	/********** ********** ********** ********** ********** ********** ********** ********** ********** **********/
-	/* define engine instance from gin framework
-	/********** ********** ********** ********** ********** ********** ********** ********** ********** **********/
+	/* define engine instance from gin framework */
 	engine := gin.Default()
 	if appConfig.Env == "production" {
 		gin.SetMode(gin.ReleaseMode)
 	}
+	middlewares(engine)
 
-	/********** ********** ********** ********** ********** ********** ********** ********** ********** **********/
-	/* define gin middleware
-	/********** ********** ********** ********** ********** ********** ********** ********** ********** **********/
-	engine.Use(gzip.Gzip(gzip.DefaultCompression))
-	engine.Use(cors.New(cors.Config{
+	/* define routes */
+	engine.StaticFile("/favicon.ico", "./resource/media/favicon.ico")
+	engine.NoRoute(defaultHandler.RouteNotFound)
+	engine.HandleMethodNotAllowed = true
+	engine.NoMethod(defaultHandler.MethodNotFound)
+	engine.GET("/", defaultHandler.Home)
+	engine.GET("/check/sentry", defaultHandler.Sentry)
+
+	if blogHandler != nil {
+		engine.GET("/blogs", blogHandler.Fetch)
+	}
+
+	/* running server using engine instance from gin framework */
+	if err := engine.Run(fmt.Sprintf("%s:%s", appConfig.Host, appConfig.Port)); err != nil {
+		logger.Error(err)
+	}
+}
+
+func middlewares(e *gin.Engine) {
+	e.Use(gzip.Gzip(gzip.DefaultCompression))
+
+	e.Use(cors.New(cors.Config{
 		AllowOrigins:     []string{"*"},
 		AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE", "HEAD"},
 		AllowHeaders:     []string{"Origin", "Content-Length", "Content-Type"},
@@ -85,7 +63,8 @@ func main() {
 		AllowCredentials: true,
 		MaxAge:           2 * time.Hour,
 	}))
-	engine.Use(func(c *gin.Context) {
+
+	e.Use(func(c *gin.Context) {
 		c.Writer.Header().Set("X-Content-Type-Options", "nosniff")
 		c.Writer.Header().Set("X-DNS-Prefetch-Control", "off")
 		c.Writer.Header().Set("X-Frame-Options", "DENY")
@@ -94,26 +73,4 @@ func main() {
 		c.Writer.Header().Set("X-XSS-Protection", "1; mode=block")
 		c.Writer.Header().Set("Referrer-Policy", "no-referrer")
 	})
-
-	/********** ********** ********** ********** ********** ********** ********** ********** ********** **********/
-	/* define routes
-	/********** ********** ********** ********** ********** ********** ********** ********** ********** **********/
-	engine.StaticFile("/favicon.ico", "./resource/media/favicon.ico")
-	engine.NoRoute(defaultHandler.RouteNotFound)
-	engine.HandleMethodNotAllowed = true
-	engine.NoMethod(defaultHandler.MethodNotFound)
-	engine.GET("/", defaultHandler.Home)
-	engine.GET("/check/sentry", defaultHandler.Sentry)
-
-	br := engine.Group("/blogs")
-	{
-		if blogHandler != nil {
-			br.GET("", blogHandler.Fetch)
-		}
-	}
-
-	/********** ********** ********** ********** ********** ********** ********** ********** ********** **********/
-	/* running server using engine instance from gin framework
-	/********** ********** ********** ********** ********** ********** ********** ********** ********** **********/
-	engine.Run(fmt.Sprintf("%s:%s", appConfig.Host, appConfig.Port))
 }
