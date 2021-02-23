f4723416f441f68f19186395ce2d7dbe63448d17
Shandy Siswandi <shandysiswandi@gmail.com>
2021-02-24 00:32:06 +0700

    call jsonplaceholder

--- cmd/console/main.go
+++ cmd/console/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	app "go-service-gin"
 	"go-service-gin/application"
+	"go-service-gin/infrastructure/external/jsonplaceholder"
 	"go-service-gin/interfaces/console"
 	"go-service-gin/util/logger"
 	"sync"
@@ -16,14 +17,19 @@ func main() {
 	var (
 		appData   = app.New()
 		blogLogic = appData[app.BlogLogic].(*application.BlogApplication)
+		jph       = appData[app.JSONPlaceholder].(*jsonplaceholder.JPH)
 
 		blogConsole = console.NewBlogConsole(blogLogic)
 	)
 
 	// testing. stress test
 	t := time.Now()
-	for i := 1; i <= 5; i++ {
+	for i := 1; i <= 1; i++ {
 		blogConsole.Create()
 	}
 	logger.Info(time.Since(t))
+
+	aa, ee := jph.Posts.Fetch()
+	logger.Error(ee)
+	logger.Info(aa)
 }
